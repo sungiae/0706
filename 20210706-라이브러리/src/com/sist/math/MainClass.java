5518e86f17dcd5880f2eeec0b966e674ab0e2905
박성경 <@DESKTOP-V401DVT>
2021-07-06 17:42:54 +0900

    3

--- 20210706-라이브러리/src/com/sist/math/MainClass.java
+++ 20210706-라이브러리/src/com/sist/math/MainClass.java
@@ -1,12 +1,30 @@
 package com.sist.math;
 // 수학 관련 (게임, 통계) => random() : 예매 가능 날, => 난수 (임의의 수)
 // ceil() => 올림 => 페이지 나누기 (총페이지)
-// 10개를 한페이지
+// 10개를 한페이지 => 총갯수가 25 ==> 25/10 => 2 ==> 25/10.0 => 2.5 => 올림 => 3
+// 기술면접 => 페이징 기법
 public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int rowSize=10; // 한페이지에 출력할 갯수
+		int count=37;
+		
+		/*
+		int total=count/rowSize; //3
+		if(count%rowSize>0)
+			total++;
+		System.out.println("총페이지:"+total);
+		*/
+		int total=(int)Math.ceil(count/(double)rowSize);
+//								 ======================= 37 / 10.0 ==> (int)3.7 => 3
+//								 ====== 올림함수 사용 ===== 37 / 10.0 ==> (int)4 => 4
+//								 반올림이 아닌 0이 아니기만 하면 올림 (3.1 => 4)
+		System.out.println("총페이지"+total);
+		for(int i=1; i<=total;i++) {
+			System.out.print(i+" ");
+		}
+		
 	}
 
 }
---
+++ 20210706-라이브러리/src/com/sist/math/MainClass2.java
@@ -0,0 +1,34 @@
+package com.sist.math;
+/*
+ *   웹에서 사용하는 자바 3대 클래스
+ *   ========================== 1. String  2. Integer  3. ArrayList
+ *   Wrapper 클래스
+ *   => 기본데이터형 (int, long, char, boolean, double, float, byte, short)
+ *      편하게 다루기 위해서 클래스형으로 제작
+ *      int a=10; => 이진법 => Integer
+ *      
+ *      정수형
+ *        byte ==== Byte
+ *        short === Short
+ *        ***int  ==== Integer
+ *        long ==== Long
+ *        char ==== Character
+ *        float === Float
+ *        ***double == Double
+ *        ***boolean = Boolean
+ *                    사용자 : 나이 "30"
+ *        웹(윈도우)  ===================> int age = ("30" ==> 정수변환)
+ *                                           (int)"30" => 불가능 int a=(int){"3","0"} (X)
+ *                                        >> 문자열을 정수형으로 변경하는 메소드
+ *                                           Integer.parseInt("30") ==> int a=30
+ *                                           "true" ==> Boolean.parseBoolean("true") => true
+ *                                           "10.5" ==> Double.parseDouble("10.5") => 10.5
+ */
+public class MainClass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
